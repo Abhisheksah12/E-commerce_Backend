94f5008057a98b40282e442906cfe995895ab733
Abhisheksah12 <aarav12sah@gmail.com>
2023-08-28 17:05:23 +0530

    User,Cart,Order Added

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const express = require("express");
 const server = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const { createProduct } = require("./controller/Product");
 const productsRouters = require("./routes/Products");
-const brandRouter = require('./routes/Brands')
-const categoriesRouter = require('./routes/Categories')
-const cors = require('cors')
-
+const brandRouter = require("./routes/Brands");
+const categoriesRouter = require("./routes/Categories");
+const userRouter = require("./routes/Users");
+const authRouter = require("./routes/Auth");
+const cartRouter = require("./routes/Cart");
+const orderRouter = require("./routes/Order");
 
 // middlewares
-server.use(cors({
-  exposedHeaders:['X-Total-Count']
-}))
+server.use(
+  cors({
+    exposedHeaders: ["X-Total-Count"],
+  })
+);
 server.use(express.json()); // to parse req.body
 server.use("/products", productsRouters.router);
 server.use("/brands", brandRouter.router);
 server.use("/categories", categoriesRouter.router);
-
+server.use("/users", userRouter.router);
+server.use("/auth", authRouter.router);
+server.use("/cart", cartRouter.router);
+server.use("/orders", orderRouter.router);
 
 //database setup
 main().catch((err) => console.log(err));
@@ -25,8 +34,6 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
   console.log("Database Connected");
 }
-
-
 
 server.get("/", (req, res) => {
   res.json({ status: "success" });
