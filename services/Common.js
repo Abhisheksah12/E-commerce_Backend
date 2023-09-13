55c50138fd2754f326225997f7ec6f2f3b96423e
Abhisheksah12 <aarav12sah@gmail.com>
2023-09-13 17:52:53 +0530

    Connected Auth with React with Cookie

--- services/Common.js
+++ services/Common.js
@@ -1,9 +1,20 @@
-const passport = require('passport');
+const passport = require("passport");
 
 exports.isAuth = (req, res, done) => {
-  return passport.authenticate('jwt')
+  return passport.authenticate("jwt");
 };
 
-exports.sanitizeUser = (user)=>{
-    return {id:user.id, role:user.role}
-}
+exports.sanitizeUser = (user) => {
+  return { id: user.id, role: user.role };
+};
+
+exports.cookieExtractor = function (req) {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies["jwt"];
+  }
+  //TODO : this is temporary token for testing without cookie
+  token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MDFhODhmZmIwMDI4ODgwMjNjMzM4MCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjk0NjA3NTAzfQ.nRacni7pdar5JeJV1jC53rn3ZqisfBGMy200tUXkxeY";
+  return token;
+};
